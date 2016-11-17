3b72d2b0ef3993714de89a82335254b06797a282
Matthew Scofield <matthewgscofield@outlook.com>
2016-11-17 12:50:37 -0600

    game converted to loop exercise

--- src/Game.java
+++ src/Game.java
@@ -16,7 +16,37 @@ public class Game {
         player.chooseName();
         player.chooseWeapon();
         player.chooseLocation();
+        player.findItem("Sheild");
+        player.findItem("Boots");
+        player.findItem("Belt");
 
+        while(true) {
+            nextLine();
+        }
 
+    }
+    //scanner with switch statement to allow user menu commands
+    public static String nextLine(){
+        String line = scanner.nextLine();
+        while   (line.startsWith("/")){
+            switch (line) {
+                case "/inv":
+                    for(String item : player.getItems()){
+                        System.out.println(item);
+                    }
+                    break;
+                case "/exit":
+                    System.exit(0);
+                    break;
+                case "/weapon":
+                    System.out.println(player.getWeapon());
+                    break;
+                default:
+                    System.out.println("you're dead to me, more dead than my dead cat");
+                    break;
+            }
+            line= scanner.nextLine();
+        }
+        return line;
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * Created by scofieldservices on 11/16/16.
  */
@@ -6,17 +8,34 @@ public class Player {
     String name;
     String weapon;
     String location;
+    ArrayList<String> items = new ArrayList();
+
+    public void findItem(String item){
+        System.out.println("You found a " + item +"! Pick it up? [y/n]");
+        String answer = Game.nextLine();
+        if(answer.equalsIgnoreCase("y")){
+            items.add(item);
+                    System.out.println("You picked up an item!");
+    }   else {
+            System.out.println("Are you sure you want to discard " + item + "? [y/n]");
+            answer = Game.nextLine();
+            if(answer.equals("n")){
+                items.add(item);
+                System.out.println("You picked up an item!");
+            }
+        }
+    }
 
     public void chooseName() {
         System.out.println("What is your name?");
-        name = Game.scanner.nextLine();
+        name = Game.nextLine();
         System.out.println("Welcome, " + name);
 
     }
 
     public void chooseWeapon() throws Exception {
     System.out.println("Choose your weapon [sword/mace]");
-    weapon = Game.scanner.nextLine();
+    weapon = Game.nextLine();
 
         if(weapon.equalsIgnoreCase("sword")) {
         System.out.println("A sword is a fine choice!");}
@@ -28,7 +47,7 @@ public class Player {
 
     public void chooseLocation() throws Exception {
         System.out.println("Choose your location [forest/tunnel]");
-        location = Game.scanner.nextLine();
+        location = Game.nextLine();
 
         if(location.equalsIgnoreCase("forest")) {
             System.out.println("Entering forest...");}
@@ -60,6 +79,14 @@ public class Player {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public ArrayList<String> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<String> items) {
+        this.items = items;
     }
 }
 
